4507de44b3302d2f277f5a253d096d77184de80b
weiwen chen <weiwenchen2022@163.com>
2024-11-12 10:42:52 +0800

    paasio

---
+++ paasio/src/lib.rs
@@ -0,0 +1,88 @@
+use std::io::{Read, Result, Write};
+
+// the PhantomData instances in this file are just to stop compiler complaints
+// about missing generics; feel free to remove them
+
+pub struct ReadStats<R> {
+    rd: R,
+    bytes_through: usize,
+    reads: usize,
+}
+
+impl<R: Read> ReadStats<R> {
+    // _wrapped is ignored because R is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: R) -> ReadStats<R> {
+        Self {
+            rd: wrapped,
+            bytes_through: 0,
+            reads: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &R {
+        &self.rd
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn reads(&self) -> usize {
+        self.reads
+    }
+}
+
+impl<R: Read> Read for ReadStats<R> {
+    fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
+        self.reads += 1;
+        let n = self.rd.read(buf)?;
+        self.bytes_through += n;
+        Ok(n)
+    }
+}
+
+pub struct WriteStats<W> {
+    wr: W,
+    bytes_through: usize,
+    writes: usize,
+}
+
+impl<W: Write> WriteStats<W> {
+    // _wrapped is ignored because W is not bounded on Debug or Display and therefore
+    // can't be passed through format!(). For actual implementation you will likely
+    // wish to remove the leading underscore so the variable is not ignored.
+    pub fn new(wrapped: W) -> WriteStats<W> {
+        Self {
+            wr: wrapped,
+            bytes_through: 0,
+            writes: 0,
+        }
+    }
+
+    pub fn get_ref(&self) -> &W {
+        &self.wr
+    }
+
+    pub fn bytes_through(&self) -> usize {
+        self.bytes_through
+    }
+
+    pub fn writes(&self) -> usize {
+        self.writes
+    }
+}
+
+impl<W: Write> Write for WriteStats<W> {
+    fn write(&mut self, buf: &[u8]) -> Result<usize> {
+        self.writes += 1;
+        let n = self.wr.write(buf)?;
+        self.bytes_through += n;
+        Ok(n)
+    }
+
+    fn flush(&mut self) -> Result<()> {
+        self.wr.flush()
+    }
+}
