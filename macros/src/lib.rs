1491369308cd9defe33feda7355970e27313e244
weiwen chen <weiwenchen2022@163.com>
2024-11-13 12:32:49 +0800

    macros

---
+++ macros/src/lib.rs
@@ -0,0 +1,17 @@
+pub use std::collections::HashMap;
+
+#[macro_export]
+macro_rules! hashmap {
+    () => {
+        $crate::HashMap::new()
+    };
+
+    ($($k:expr => $v:expr),+ $(,)?) => {{
+        let mut hm = $crate::HashMap::new();
+        $(
+            hm.insert($k, $v);
+        )*
+
+        hm
+    }};
+}
